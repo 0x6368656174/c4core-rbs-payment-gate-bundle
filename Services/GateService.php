5724a0abaae0d4a8fa2aac032e4c902d5e8127fb
0x6368656174 <0x6368656174@gmail.com>
2017-03-27 09:09:54 +1000

    Добавлено запоминание клиента

--- Services/GateService.php
+++ Services/GateService.php
@@ -228,11 +228,12 @@ class GateService extends Gate
         $registerOrderRequest->setDescription($order->getDescription());
         $registerOrderRequest->setAmount($order->getAmount() * 100);
         $registerOrderRequest->setSessionTimeoutSecs($this->paySessionTimeoutSecs);
+        $registerOrderRequest->setClientId($order->getCustomer()->getId());
 
         if (key_exists('merchantLogin', $extraParameters))
             $registerOrderRequest->setMerchantLogin($extraParameters['merchantLogin']);
-        //if (key_exists('pageView', $extraParameters))
-        //    $registerOrderRequest->setPageView($extraParameters['pageView']);
+//        if (key_exists('pageView', $extraParameters))
+//            $registerOrderRequest->setPageView($extraParameters['pageView']);
 
         $routeParams = array(
             'pageView' => $registerOrderRequest->getPageView()
